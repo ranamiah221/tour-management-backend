782ec7c96d7778af25fd57d4aca0aef81243622a
ranamiah221 <ranarasul21@example.com>
2025-07-10 15:19:12 +0600

    setup project

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response } from 'express'
+
+const app:Application = express();
+
+app.get('/', async(req:Request, res:Response)=>{
+  res.send("Welcome to tour management system")
+})
+
+export default app;
