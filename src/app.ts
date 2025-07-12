99f49b64552b3ac31f9c81f03ca6d1ecf2fcc2c2
ranamiah221 <ranarasul21@example.com>
2025-07-13 00:18:24 +0600

    added login routes for login and create update user role and implement zod for extra validation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { router } from './app/routes';
+import { globalErrorHandler } from './app/middleware/globalErrorHandler';
+import notFound from './app/middleware/notFound';
 
 const app:Application = express();
+app.use(express.json())
+app.use(cors())
+
+
+app.use('/api/v1', router)
 
 app.get('/', async(req:Request, res:Response)=>{
   res.send("Welcome to tour management system")
 })
+
+app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app;
